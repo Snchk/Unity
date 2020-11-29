5f16156eac8d7501614c003737fd8bbb60c0bb5e
Alexandr <cawa2016@gmail.com>
2020-11-29 22:36:03 +0200

    Add game

---
+++ Assets/Hero Knight - Pixel Art/Scripts/Player/ItemPicker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPicker : MonoBehaviour
+{
+    [SerializeField] private int _healvalue;
+
+    private void OnTriggerEnter2D(Collider2D info)
+    {
+        info.GetComponent<Player_controller>().RestoreHP(_healvalue);
+       Destroy(gameObject);
+    }
+    // Start is called before the first frame update
+   
+}
