26764277563308837ff8b16ee122764b8af92c80
Snchk <cawa2016@gmail.com>
2021-04-14 00:39:43 +0300

    Add 3d

---
+++ 3D/Assets/Script/LivingCreature.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Rigidbody),typeof(Collider),typeof(Animator))]
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class LivingCreature : MonoBehaviour
+{
+
+    public Rigidbody CreatureRB { get; private set; }
+    public Collider CreatureCollider { get; private set; }
+    public NavMeshAgent CreatureNavMeshAgent { get; private set; }
+    public Animator CreatureAnimator { get; private set; }
+
+   // [SerialezeField] private LivingCreatureActionController _actionController;
+   // public LivingCreatureActionController => _actionController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreatureRB = GetComponent<Rigidbody>();
+        CreatureCollider = GetComponent<Collider>();
+        CreatureNavMeshAgent = GetComponent<NavMeshAgent>();
+        CreatureAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 3D/Assets/Script/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private NavMeshAgent _navMeshAgent;
+    private Camera _cam;
+    private bool _leftPointerClicked;
+    private Animator _playerAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cam = Camera.main;
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _leftPointerClicked = Input.GetButton("Fire1");
+    }
+
+    private void FixedUpdate()
+    {
+        if (_leftPointerClicked)
+        {
+            RaycastHit hitInfo;
+            if (Physics.Raycast(_cam.ScreenPointToRay(Input.mousePosition), out hitInfo, 100))
+            {
+                _navMeshAgent.destination = hitInfo.point;
+                _playerAnimator.SetBool("Run", true);
+            }
+        }
+        if (Vector3.Distance(transform.position, _navMeshAgent.destination) <= _navMeshAgent.stoppingDistance && _playerAnimator.GetBool("Run")) 
+        {
+            _playerAnimator.SetBool("Run", false);
+            _navMeshAgent.destination = transform.position;
+        }
+    }
+}
